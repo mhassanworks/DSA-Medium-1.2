7d9b15d9129bddecd8c58085d0579eb50d1a8549
MOHAMMAD HASSAN <159164628+mhassanworks@users.noreply.github.com>
2025-06-14 22:10:30 +0530

    Create Array.cpp

---
+++ Array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare and initialize array
+    int numbers[5] = {10, 20, 30, 40, 50};
+
+    // Print all elements
+    for (int i = 0; i < 5; i++) {
+        cout << "Element at index " << i << " is " << numbers[i] << endl;
+    }
+
+    return 0;
+}
